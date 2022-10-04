fc386691ca1808b77fb1684d741498e69a36569a
Jstaples <jstaplesmusic@gmail.com>
2022-10-04 18:29:51 -0400

    Added js script file and began working on functions to change colors of squares

---
+++ projects/color-squares/color-squares.js
@@ -0,0 +1,21 @@
+// *GLOBAL VARIABLES*
+
+var colorsArray = ['neon-green', 'dark-green', 'hot-pink', 'dark-pink']
+
+var buttonsElArray = $('li');
+buttonsElArray.onClick('click', assignRandomSquare())
+
+
+// *FUNCTIONS*
+
+function assignRandomSquare() {
+    var squareArray = $('.square')
+
+}
+
+
+// *TESTS*
+
+
+
+// *EXECUTION*
