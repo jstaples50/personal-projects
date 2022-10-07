bc87918291e364b3ada802520d1bb24bf174cde7
Jstaples <jstaplesmusic@gmail.com>
2022-10-07 17:47:10 -0400

    Created functions to add rows based on user input and remove the input html

--- projects/color-squares/color-squares.js
+++ projects/color-squares/color-squares.js
@@ -3,9 +3,16 @@
 var colorsArray = ['neon-green', 'dark-green', 'hot-pink', 'dark-pink']
 
 var buttonsElArray = $('li');
-$(buttonsElArray).on('click', assignRandomSquare)
+$(buttonsElArray).on('click', assignRandomSquare);
 
-var squareArray = $('.square')
+var squareArray = ''
+
+var submitBtnEl = $('#submit-btn');
+$(submitBtnEl).on('click', askUserforNumberOfRows);
+
+var squareGrid = $('#square-grid');
+
+var userInputRows = '';
 
 // *FUNCTIONS*
 
@@ -17,10 +24,39 @@ function assignRandomSquare() {
     }
 }
 
+function askUserforNumberOfRows() {
+    userInputRows = $('input').val();
+    createSquareGrid();
+}
+
+function removeRowSelector() {
+    $('#row-selector').empty();
+    $('#row-selector').detach();
+}
+
+function createSquareGrid() {
+    var squareID = 1;
+    for (var i = 0; i < userInputRows; i++) {
+        var rowEl = document.createElement('div');
+        $(squareGrid).append(rowEl);
+        $(rowEl).addClass(['row', 'd-flex', 'justify-space-around']);
+        for (var j = 0; j < 5; j++) {
+            var squareEl = document.createElement('div', {is: `${squareID}`});
+            $(squareEl).addClass(['col', 'd-flex', 'square', 'justify-align-center']);
+            $(squareEl).text(`${squareID}`);
+            $(rowEl).append(squareEl);
+            squareID++
+        }
+    }
+    squareArray = $('.square');
+    removeRowSelector();
+}
 
 // *TESTS*
-console.log(buttonsElArray)
-console.log(colorsArray[0])
-console.log(squareArray[1])
+
+function testEventListener() {
+    console.log('test');
+}
+
 
 // *EXECUTION*
